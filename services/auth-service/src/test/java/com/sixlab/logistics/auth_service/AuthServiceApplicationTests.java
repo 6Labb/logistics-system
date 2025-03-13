79757a36d5a7896b164fe8576c99e11acbde7003
thegreathnb <thegreathnb@kakao.com>
2025-03-13 14:58:13 +0900

    Initial commit

---
+++ services/auth-service/src/test/java/com/sixlab/logistics/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sixlab.logistics.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ services/company-service/src/main/java/com/sixlab/logistics/company_service/CompanyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sixlab.logistics.company_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyServiceApplication.class, args);
+	}
+
+}
