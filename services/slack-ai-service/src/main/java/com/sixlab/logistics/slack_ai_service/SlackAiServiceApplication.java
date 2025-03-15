a93d284ce36b6bdfb8cbac876ab1971f96615061
thegreathnb <thegreathnb@kakao.com>
2025-03-15 13:43:08 +0900

    CHORE: 인프라 설정파일 추가

---
+++ services/slack-ai-service/src/main/java/com/sixlab/logistics/slack_ai_service/SlackAiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sixlab.logistics.slack_ai_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackAiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlackAiServiceApplication.class, args);
+	}
+
+}
