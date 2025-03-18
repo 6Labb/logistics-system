4048239f88307a856b0a173458e41279c5caf9b6
goodperiodt <ilypsj@gmail.com>
2025-03-19 03:51:01 +0900

    FEAT: 주문 생성 로직 작성 및 테스트 완료_1차

--- services/order-service/src/main/java/com/sixlab/logistics/order_service/application/client/UserClient.java
+++
@@ -1,7 +0,0 @@
-package com.sixlab.logistics.order_service.application.client;
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(name="user-service")
-public interface UserClient {
-}
