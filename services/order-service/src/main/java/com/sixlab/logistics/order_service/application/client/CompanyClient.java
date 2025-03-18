dac830dd2ffea412079ab137ea924fefed40bae0
goodperiodt <ilypsj@gmail.com>
2025-03-18 17:13:48 +0900

    FEAT: FeignClient interface 작성

--- services/order-service/src/main/java/com/sixlab/logistics/order_service/application/client/CompanyClient.java
+++ services/order-service/src/main/java/com/sixlab/logistics/order_service/application/client/CompanyClient.java
@@ -1,16 +1,16 @@
 package com.sixlab.logistics.order_service.application.client;
 
 import com.sixlab.logistics.common.shared.response.ApiResponse;
+import com.sixlab.logistics.order_service.application.dto.response.GetCompanyResponseDto;
 import com.sixlab.logistics.order_service.application.dto.response.GetProductResponseDto;
-import org.hibernate.validator.constraints.UUID;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="product-service")
-public interface ProductClient {
-    // product 조회 (param 은 productId)
-    // 1. RequestMapping("/products") 있는지 확인할 것
-    @GetMapping("/{productId}")
-    ApiResponse<GetProductResponseDto> getProductById(@PathVariable UUID productId);
+import java.util.UUID;
+
+@FeignClient(name="company-service")
+public interface CompanyClient {
+    @GetMapping("/{companyId}")
+    ApiResponse<GetCompanyResponseDto> getCompanyById(@PathVariable UUID companyId);
 }
--- services/order-service/src/main/java/com/sixlab/logistics/order_service/application/client/DeliveryClient.java
+++ services/order-service/src/main/java/com/sixlab/logistics/order_service/application/client/DeliveryClient.java
@@ -1,8 +1,15 @@
 package com.sixlab.logistics.order_service.application.client;
 
+import com.sixlab.logistics.common.shared.response.ApiResponse;
+import com.sixlab.logistics.order_service.application.dto.request.RequestDeliveryRegisterDto;
+import com.sixlab.logistics.order_service.application.dto.response.GetProductResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name="delivery-service")
 public interface DeliveryClient {
     // 배송 생성 메서드 호출
+    @GetMapping
+    ApiResponse<GetProductResponseDto> getProductById(@RequestBody RequestDeliveryRegisterDto dto);
 }
