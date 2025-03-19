85643fd2b1436a9357468753837f5399ab393bd9
mummumm <01088287493@daum.net>
2025-03-19 12:37:25 +0900

    FEAT : delivery, deliveryRoute entity, dto 내용변경

--- services/delivery-service/src/main/java/com/sixlab/logistics/delivery_service/application/dto/DeliveryRequestDto.java
+++ services/delivery-service/src/main/java/com/sixlab/logistics/delivery_service/application/dto/DeliveryRequestDto.java
@@ -14,11 +14,11 @@ public class DeliveryRequestDto {
     private DeliveryStatus status;
     private String deliveryAddress;
     private String receiveName;
-    private UUID companyDeliveryAgentId;
+    private UUID companyDeliveryAgentId; // 업체배송담당자
     private UUID hubTotalRouteId;
     private UUID fromHubId;
     private UUID toHubId;
-    // private UUID 공급업체 ID;
-    // private UUID 수령업체 ID;
+    private UUID receiverCompanyId; // 수령업체 id
+    private UUID supplierCompanyId; // 공급업체 id
 
 }
