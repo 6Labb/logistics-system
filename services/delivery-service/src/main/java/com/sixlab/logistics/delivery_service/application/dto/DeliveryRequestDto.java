90571918310d4419c221d98d9b7429870370e579
mummumm <01088287493@daum.net>
2025-03-19 15:08:10 +0900

    FEAT : requestDTO 파일 수정

--- services/delivery-service/src/main/java/com/sixlab/logistics/delivery_service/application/dto/DeliveryRequestDto.java
+++ services/delivery-service/src/main/java/com/sixlab/logistics/delivery_service/application/dto/DeliveryRequestDto.java
@@ -10,14 +10,8 @@ import java.util.UUID;
 @NoArgsConstructor
 public class DeliveryRequestDto {
 
-    private UUID id;
-    private DeliveryStatus status;
     private String deliveryAddress;
     private String receiveName;
-    private Long deliveryAgentId; // 업체배송담당자
-    private UUID hubTotalRouteId;
-    private UUID fromHubId;
-    private UUID toHubId;
     private UUID receiverCompanyId; // 수령업체 id
     private UUID supplierCompanyId; // 공급업체 id
 
