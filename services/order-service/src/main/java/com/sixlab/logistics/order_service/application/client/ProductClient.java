f39612935e9aa4745686f957bf605e331da41246
goodperiodt <ilypsj@gmail.com>
2025-03-21 12:19:41 +0900

    REFACT: order-service 배송 등록 기능 develop, refactoring, dto 변경사항 반영

--- services/order-service/src/main/java/com/sixlab/logistics/order_service/application/client/ProductClient.java
+++ services/order-service/src/main/java/com/sixlab/logistics/order_service/application/client/ProductClient.java
@@ -1,17 +1,33 @@
 package com.sixlab.logistics.order_service.application.client;
 
-import com.sixlab.logistics.common.shared.response.ApiResponse;
+import com.sixlab.logistics.common.shared.response.ApiResponseDto;
 import com.sixlab.logistics.order_service.application.dto.response.GetProductResponseDto;
 
+import com.sixlab.logistics.order_service.application.dto.response.ProductStockResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
 @FeignClient(name="product-service")
+// @RequestMapping("/products") 여기에 @RequestMapping 어노테이션 사용해도 되나?
 public interface ProductClient {
     // product 조회 (param 은 productId)
     @GetMapping("/products/{productId}")
-    ApiResponse<GetProductResponseDto> getProductById(@PathVariable UUID productId);
+    ResponseEntity<ApiResponseDto<GetProductResponseDto>> getProductById(@PathVariable UUID productId);
+
+    // 상품 수량 감소 요청
+    @PutMapping("products/{productId}/decrease-stock")
+    ResponseEntity<ApiResponseDto<ProductStockResponseDto>> requestProductStockDecrease(@PathVariable UUID productId,
+                                                                                        @RequestParam int quantity);
+
+    // 상품 수량 복원 요청
+    @PutMapping("products/{productId}/restore-stock")
+    ResponseEntity<ApiResponseDto<ProductStockResponseDto>> requestProductStockRestore(@PathVariable UUID productId,
+                                                                                        @RequestParam int quantity);
+
+
+
+
 }
