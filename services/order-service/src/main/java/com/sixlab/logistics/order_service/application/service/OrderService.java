cdeff193377862366487484539ac0dd4aae46af6
goodperiodt <ilypsj@gmail.com>
2025-03-22 20:44:16 +0900

    commit

--- services/order-service/src/main/java/com/sixlab/logistics/order_service/application/service/OrderService.java
+++ services/order-service/src/main/java/com/sixlab/logistics/order_service/application/service/OrderService.java
@@ -1,6 +1,5 @@
 package com.sixlab.logistics.order_service.application.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sixlab.logistics.common.shared.exception.OutOfStockException;
 import com.sixlab.logistics.common.shared.exception.ResourceNotFoundException;
@@ -11,6 +10,7 @@ import com.sixlab.logistics.order_service.application.client.ProductClient;
 import com.sixlab.logistics.order_service.application.dto.UserInfo;
 import com.sixlab.logistics.order_service.application.dto.request.OrderCreateRequestDto;
 
+import com.sixlab.logistics.order_service.application.dto.request.OrderInfoMessageRequestDto;
 import com.sixlab.logistics.order_service.application.dto.request.OrderInfoMessageRequestDto;
 import com.sixlab.logistics.order_service.application.dto.request.OrderInfoUpdateRequestDto;
 import com.sixlab.logistics.order_service.application.dto.request.RequestDeliveryRegisterDto;
@@ -65,12 +65,12 @@ public class OrderService {
     @Value("${message.queue.order}")
     private String queueOrder;
 
-    public OrderCreateResponseDto createOrder(OrderCreateRequestDto dto) throws Exception{
-        log.info("service 계층: createOrder() 호출됨");
-        // 1. 모든 로그인 사용자 (주문자) 가능
-        // jwt 를 통해 user 의 id 를 얻어낸다.
+    // 모든 권한 접근 허용, 주문 생성 메서드
+    public OrderCreateResponseDto createOrder(OrderCreateRequestDto dto, Long userId) throws Exception{
+        // log.info("service 계층: createOrder() 호출됨");
 
-        // 2. 상품서비스의 상품 조회기능 호출
+
+        // 1. 상품서비스의 상품 조회기능 호출
         ResponseEntity<ApiResponseDto<GetProductResponseDto>> requestProduct = productClient.getProductById(dto.getProductId());
 
         /* 상기코드 실행시 전달받을 객체, 지금은 Product 객체 직접 생성
