f8556abb30aa1b2f803d45f54e89d4c9411dd74a
Hooni-i <rlagns0904@gmail.com>
2025-03-22 19:03:50 +0900

    init: config 변경

--- services/slack-ai-service/src/main/java/com/sixlab/logistics/slack_ai_service/Messenger/config/SecurityConfig.java
+++ services/slack-ai-service/src/main/java/com/sixlab/logistics/slack_ai_service/Messenger/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.sixlab.logistics.slack_ai_service.Messenger.config;
-
+import com.sixlab.logistics.common.shared.security.JwtAuthenticationFilter;
+import com.sixlab.logistics.common.shared.security.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -11,20 +12,19 @@ import org.springframework.security.core.context.SecurityContextHolderStrategy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-
-/**
- *  Spring Security를 적용하여 jwt 기반 인증을 활성화하는 역할
- *  모든 요청이 들어올때 JwtAuthenticationFilter를 적용하여 Jwt가 유효한지 검증
- */
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
 
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    @Bean(name = "jwtTokenProvider")
+    public JwtTokenProvider jwtTokenProvider() {
+        return new JwtTokenProvider();
+    }
 
-    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
-        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter(jwtTokenProvider());
     }
 
     @Bean
@@ -40,7 +40,7 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 // JWT 필터 추가
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
         // 설정된 보안 필터 체인을 반환합니다.
         return http.build();
@@ -51,8 +51,5 @@ public class SecurityConfig {
         SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
         return SecurityContextHolder.getContextHolderStrategy();
     }
-
-
 }
-
 
