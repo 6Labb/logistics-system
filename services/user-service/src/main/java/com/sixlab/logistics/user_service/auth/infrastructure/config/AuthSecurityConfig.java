0aefb4ed1e4d6c8fa7052418c1ddc310b90ccb94
thegreathnb <thegreathnb@kakao.com>
2025-03-23 23:05:07 +0900

    CHORE : x

--- services/user-service/src/main/java/com/sixlab/logistics/user_service/auth/infrastructure/config/AuthSecurityConfig.java
+++ services/user-service/src/main/java/com/sixlab/logistics/user_service/auth/infrastructure/config/AuthSecurityConfig.java
@@ -3,9 +3,12 @@ package com.sixlab.logistics.user_service.auth.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.context.SecurityContextHolderStrategy;
@@ -32,12 +35,25 @@ public class AuthSecurityConfig {
                 // CSRF 보호를 비활성화합니다. CSRF 보호는 주로 브라우저 클라이언트를 대상으로 하는 공격을 방지하기 위해 사용됩니다.
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize // 요청에 대한 접근 권한을 설정합니다.
-                        // /auth/signIn 경로에 대한 접근을 허용합니다. 이 경로는 인증 없이 접근할 수 있습니다.
-                        .requestMatchers("/auth/signIn").permitAll()
-                        .requestMatchers("/users/signUp").permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers(
+                                "/auth/signIn",
+                                "/users/signUp",
+                                "/auth/v3/api-docs",
+                                "/auth/v3/api-docs/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/swagger-resources/**",
+                                "/swagger-resources",
+                                "/swagger-resources/configuration/ui",
+                                "/swagger-resources/configuration/security",
+                                "/webjars/**"
+                        ).permitAll()
                         // 그 외의 모든 요청은 인증이 필요합니다.
                         .anyRequest().authenticated()
                 )
+                .anonymous(Customizer.withDefaults())
                 // 세션 관리 정책을 정의합니다. 여기서는 세션을 사용하지 않도록 STATELESS로 설정합니다. 지금은 jwt 사용할 것이기 때문.
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -60,4 +76,24 @@ public class AuthSecurityConfig {
         SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
         return SecurityContextHolder.getContextHolderStrategy();
     }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers(
+                "/auth/v3/api-docs",
+                "/auth/v3/api-docs/**",
+                "/users/v3/api-docs",
+                "/users/v3/api-docs/**",
+                "/v3/api-docs/**",
+                "/swagger-ui.html",
+                "/swagger-ui/**",
+                "/swagger-resources/**",
+                "/swagger-resources",
+                "/swagger-resources/configuration/ui",
+                "/swagger-resources/configuration/security",
+                "/webjars/**"
+        );
+    }
+
+
 }
